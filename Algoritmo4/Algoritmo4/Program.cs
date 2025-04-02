c011581f43427a792ad80dbef050b389730a94b4
Miguel <poleom2@gmail.cpm>
2025-04-01 21:36:16 -0300

    conclucion del ejecicio 4

--- Algoritmo4/Algoritmo4/Program.cs
+++ Algoritmo4/Algoritmo4/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+double largura;
+double comprimeto, area, valormetro, valorterreno;
+
+largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+comprimeto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+valormetro = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+area = largura * comprimeto;
+valorterreno = area * valormetro;
+Console.WriteLine("AREA - " + area.ToString("F2",CultureInfo.InvariantCulture));
+Console.WriteLine("PRECIO = " + valorterreno.ToString("F2",CultureInfo.InvariantCulture));
+
